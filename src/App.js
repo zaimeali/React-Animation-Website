8c0f13a9611989a7bb24fd225dd2d8295ae73c48
zaime ali <zaimeali1997@gmail.com>
2020-07-31 02:08:36 +0500

    Svg Added and showing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import NavItems from './components/NavItems';
 
+// Icon
+import { ReactComponent as Caret } from './img/icons/caret.svg';
+
 function App() {
   return (
     <div>
@@ -10,6 +13,10 @@ function App() {
         <NavItems icon="🏏" />
         <NavItems icon="🏏" />
         <NavItems icon="🏏" />
+
+        <NavItems icon={ <Caret /> }>
+
+        </NavItems>
       </Navbar>
     </div>
   );
